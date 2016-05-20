7f72c5ede2a3f4df25f414bb0d68fc3d308daff3
j.faassen <j.faassen@linkorb.com>
2016-05-20 17:45:15 +0200

    Use parameter on account_get

--- examples/account_get.php
+++ examples/account_get.php
@@ -2,8 +2,14 @@
 
 require_once ('common.php');
 
+if (count($argv)!=2) {
+    echo "Please pass 1 parameter: username\n";
+    exit();
+}
+
 try {
-    $account = $client->getAccountByName('joost');
+    $accountName = $argv[1];
+    $account = $client->getAccountByName($accountName);
     print_r($account);
 } catch (Exception $e) {
     echo "Exception " . $e->getMessage() . "\n";

9dac2b30becb54a0636c30e2194d8a63e67d9ffe
j.faassen <j.faassen@linkorb.com>
2019-02-12 16:32:17 +0100

    Support UserLoaded event
    can be used to set roles at runtime

---
+++ src/Event/UserLoadedEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace UserBase\Client\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use UserBase\Client\Model\User;
+
+class UserLoadedEvent extends Event
+{
+    public const NAME = 'userbase.user_loaded';
+
+    protected $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
--- src/UserProvider.php
+++ src/UserProvider.php
@@ -8,24 +8,34 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 use UserBase\Client\Model\User;
+use UserBase\Client\Event\UserLoadedEvent;
+
 
 class UserProvider implements UserProviderInterface
 {
     private $client;
     private $shouldRefresh;
+    private $dispatcher;
 
-    public function __construct(Client $client, $shouldRefresh = true)
+    public function __construct(Client $client, $shouldRefresh = true, EventDispatcherInterface $dispatcher = null)
     {
         $this->client = $client;
         $this->shouldRefresh = (bool) $shouldRefresh;
+        $this->dispatcher = $dispatcher;
     }
 
     public function loadUserByUsername($username)
     {
         try {
-            return $this->client->getUserByUsername($username);
+            $user = $this->client->getUserByUsername($username);
+            if ($this->dispatcher) {
+                $event = new UserLoadedEvent($user);
+                $this->dispatcher->dispatch('userbase.user_loaded', $event);
+            }
+            return $user;
         } catch (RuntimeException $e) {
             throw new UsernameNotFoundException(
                 "A User named \"{$username}\" cannot be found in Userbase.",
